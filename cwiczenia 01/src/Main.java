d8eef9ea1c0eeaa77eafb29573e0cb5d725c9cd6
Hanna Polishchuk <hanpol@gmail.com>
2026-09-11 09:17:28 +0200

    Instrukcje wypisania

--- cwiczenia 01/src/Main.java
+++ cwiczenia 01/src/Main.java
@@ -1,13 +1,17 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+    System.out.println("Hello world!"); //wypisz i zrob enter
+    System.out.println("Sktot"); //sout
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
+    System.out.print("Coś bez robienia entera");
+    System.out.println("Nowa linia");
+
+    System.out.println("W taki sposób robię znaki specjalne:");
+    System.out.println("\t tabulacja"); //tab
+    System.out.println("\n enter"); //enter
+    System.out.println("To jest jak na maszynie\rdo pisania");//powrot karetki
+    System.out.println("\"codzyslów\"");
+    System.out.println("\'apostrof\'");
+    System.out.println("backslash: \\");
 }
