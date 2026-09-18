21e9fcd952ad50913b7495000297aeaf0c3a14a4
Hanna Polishchuk <annapol26@gmail.com>
2026-09-18 08:43:29 +0200

    Zadanie02 - deklaracja zmiennych

--- Zadania01/src/Main.java
+++ Zadania01/src/Main.java
@@ -3,4 +3,10 @@ void main() {
     System.out.println("Ania");
     System.out.println("Bartek");
     System.out.println("Kasia");
+
+    //zad2
+    String imie= "Hanna";
+    int rokurodzenia = 2010;
+    double liczba = 0.66;
+
 }
