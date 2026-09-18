ccfc84247332f7c2b3bda27b1f90e14e4b7c0154
Hanna Polishchuk <annapol26@gmail.com>
2026-09-18 08:47:08 +0200

    Zadanie03 - Wypisanie inf o sobie

--- Zadania01/src/Main.java
+++ Zadania01/src/Main.java
@@ -8,5 +8,9 @@ void main() {
     String imie= "Hanna";
     int rokurodzenia = 2010;
     double liczba = 0.66;
-
+    //zad3
+    //zad3
+    int obecnyRok = 2026;
+    int wiek = (obecnyRok - rokurodzenia);
+    System.out.println("Mam na imię " + imie + " mam " + wiek + " lat i będę pisać maturę za " + liczba + " roku.");
 }
