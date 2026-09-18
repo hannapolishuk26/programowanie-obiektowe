dc66008165c204f20790bec1cbae69f90f38e451
Hanna Polishchuk <annapol26@gmail.com>
2026-09-18 09:13:44 +0200

    Zadanie04 - konwersja temperatury

--- Zadania01/src/Main.java
+++ Zadania01/src/Main.java
@@ -1,16 +1,28 @@
-void main() {
-    //zad1
-    System.out.println("Ania");
-    System.out.println("Bartek");
-    System.out.println("Kasia");
+import java.util.Scanner;
 
-    //zad2
-    String imie= "Hanna";
-    int rokurodzenia = 2010;
-    double liczba = 0.66;
-    //zad3
-    //zad3
-    int obecnyRok = 2026;
-    int wiek = (obecnyRok - rokurodzenia);
-    System.out.println("Mam na imię " + imie + " mam " + wiek + " lat i będę pisać maturę za " + liczba + " roku.");
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //zad1
+        System.out.println("Ania");
+        System.out.println("Bartek");
+        System.out.println("Kasia");
+
+        //zad2
+        String imie = "Hanna";
+        int rokurodzenia = 2010;
+        double liczba = 0.66;
+        //zad3
+        //zad3
+        int obecnyRok = 2026;
+        int wiek = (obecnyRok - rokurodzenia);
+        System.out.println("Mam na imię " + imie + " mam " + wiek + " lat i będę pisać maturę za " + liczba + " roku.");
+
+        //zad4
+        double stopnie;
+        System.out.println("Podaj temperature na dworze w stopniach Cel");
+        stopnie = scanner.nextDouble();
+        double fahrenheit = 1.8 * stopnie + 32.0;
+        System.out.println("Temperatura w stopniach Fahrenheita: " + fahrenheit);
+    }
 }
