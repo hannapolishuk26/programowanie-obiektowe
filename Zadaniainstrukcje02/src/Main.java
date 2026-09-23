f80595ddb8b359bad022190d89827f42ef4e4d4b
Hanna Polishchuk <annapol@gmail.com>
2026-09-23 13:54:58 +0200

    zad2 - zbudowac trojkat

--- Zadaniainstrukcje02/src/Main.java
+++ Zadaniainstrukcje02/src/Main.java
@@ -12,6 +12,19 @@ public class Main {
             System.out.println("Liczba " + liczba + " nie dzieli się przez 3");
         }
 
+        //zad2
+        System.out.println("Podaj trzy długości boków trójkąta:");
+        System.out.print("Bok a: ");
+        double a = scanner.nextDouble();
+        System.out.print("Bok b: ");
+        double b = scanner.nextDouble();
+        System.out.print("Bok c: ");
+        double c = scanner.nextDouble();
+        if (a + b > c && a + c > b && b + c > a) {
+            System.out.println("Można zbudować trójkąt.");
+        } else {
+            System.out.println("Nie można zbudować trójkąta.");
+        }
         scanner.close();
     }
 }
